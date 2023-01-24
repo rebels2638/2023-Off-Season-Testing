0e200bd90d652ca2c839a4ef0132ff1396bcd577
ejiang2 <115647009+ejiang2@users.noreply.github.com>
2023-01-23 19:41:47 -0500

    Update ElevatorPIDController.java
    fixed isFinished

--- src/main/java/frc/robot/commands/ElevatorPIDController.java
+++ src/main/java/frc/robot/commands/ElevatorPIDController.java
@@ -67,6 +67,6 @@ public class ElevatorPIDController extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return m_elevatorPID.m_controller.atGoal();
   }
 }
