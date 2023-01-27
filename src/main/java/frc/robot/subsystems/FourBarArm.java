29f5fa9f492371b84188687b122231684207602b
GNSRobotics2638 <GNSRobotics2638@outlook.com>
2023-01-27 17:51:22 -0500

    Uncommented fourbar

--- src/main/java/frc/robot/subsystems/FourBarArm.java
+++ src/main/java/frc/robot/subsystems/FourBarArm.java
@@ -1,42 +1,42 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import com.ctre.phoenix.motorcontrol.ControlMode;
-// // import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-// // import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-// import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX; 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.CANSparkMaxLowLevel;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+// import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+// import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX; 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// public class FourBarArm extends SubsystemBase {
-//     private final WPI_TalonFX victor;
-//     private final CANSparkMax spark;
+public class FourBarArm extends SubsystemBase {
+    private final WPI_TalonFX victor;
+    private final CANSparkMax spark;
     
-//     private static FourBarArm instance = null;
+    private static FourBarArm instance = null;
 
-//     public FourBarArm() {
-//         this.victor = new WPI_TalonFX(0); // one instance of TalonSRX, replaced IntakeConstants.TALON_ID
-//         this.spark = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public FourBarArm() {
+        this.victor = new WPI_TalonFX(5); // one instance of TalonSRX, replaced IntakeConstants.TALON_ID
+        this.spark = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-//     }
+    }
 
-//     // Singleton class, call getInstance to access instead of the constructor.
-//     public static FourBarArm getInstance() {
-//         if (instance == null) {
-//             instance = new FourBarArm();
-//         }
-//         return instance;
-//     }
+    // Singleton class, call getInstance to access instead of the constructor.
+    public static FourBarArm getInstance() {
+        if (instance == null) {
+            instance = new FourBarArm();
+        }
+        return instance;
+    }
 
-//     public void setPercentOutputVictor(double percent) {
+    public void setPercentOutputVictor(double percent) {
 
-//         victor.set(ControlMode.PercentOutput, percent); // set talon speed based on input from XboxController.getleftY(), ie the input range on left y should map to the speed???? where speed is in range -1,1 and the xbox controller left joy stick is also -1,1???
-//     }
+        victor.set(ControlMode.PercentOutput, percent); // set talon speed based on input from XboxController.getleftY(), ie the input range on left y should map to the speed???? where speed is in range -1,1 and the xbox controller left joy stick is also -1,1???
+    }
     
-//     public void setSpeedSpark(double speed) {
+    public void setSpeedSpark(double speed) {
         
-//         spark.set(speed);
+        spark.set(speed);
         
-//     }
+    }
     
-// }
+}
