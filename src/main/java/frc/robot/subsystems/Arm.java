683373d8f78e032191bcd8c9901733d823b0a359
GNSRobotics2638 <GNSRobotics2638@outlook.com>
2023-02-06 15:27:29 -0500

    added limits

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,16 +7,20 @@ import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
     private final WPI_TalonFX talon;
+    private final GenericEntry tab;
     private static Arm instance = null;
     private static double lastPercentSpeed; 
-    private static double kUpperLimit = 609000.0;
-    private static double kLowerLimit = -866000.0;
+    private static double kUpperLimit = 55000.0;
+    private static double kLowerLimit = -65000.0;
 
     public Arm() {
         this.talon = new WPI_TalonFX(5); // one instance of TalonSRX, replaced IntakeConstants.TALON_ID
@@ -38,6 +42,7 @@ public class Arm extends SubsystemBase {
         talon.configAllSettings(falconConfig);
         talon.setNeutralMode(NeutralMode.Brake);
         
+        tab = Shuffleboard.getTab("SmartDashboard").add("Arm Encoder", 0.0).getEntry();
     }
 
     // Singleton class, call getInstance to access instead of the constructor.
@@ -49,20 +54,7 @@ public class Arm extends SubsystemBase {
     }
 
     public void setPercentOutput(double percent) {
-        
         double currentEncoder = talon.getSensorCollection().getIntegratedSensorPosition();
-        // System.out.println(currentEncoder);
-        // System.out.println("Precent: " + percent);
-        
-        // if(Math.abs(percent) < 0.08) {
-        //     percent = 0;
-        //     //talon.enableBrakeMode(true); 
-        // }
-      
-        // if(percent == 0) {
-        //     //talon.enableBrakeMode(true);    
-        // }
-        // // System.out.println(percent);
 
         if (currentEncoder >= kUpperLimit && percent > 0.0) {
             percent = 0.0;
@@ -73,6 +65,11 @@ public class Arm extends SubsystemBase {
 
         talon.set(ControlMode.PercentOutput, percent); 
         // set talon speed based on input from XboxController.getleftY(), ie the input range on left y should map to the speed, where both the speed and the left joy stick is in range -1,1
+    }
+
+    @Override
+    public void periodic() {
+        tab.setDouble(talon.getSelectedSensorPosition());
     }
 
     public void zeroEncoder() {
