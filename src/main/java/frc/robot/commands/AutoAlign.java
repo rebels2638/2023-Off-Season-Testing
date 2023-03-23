c067fc861739ef0d053b67f910244a56daff5b35
BobdeThird <cadenli2025@gmail.com>
2023-03-23 00:15:44 -0400

    cringe eric made me change from in to cm >:(

--- src/main/java/frc/robot/commands/AutoAlign.java
+++ src/main/java/frc/robot/commands/AutoAlign.java
@@ -39,18 +39,18 @@ public class AutoAlign extends CommandBase {
         double limelightMountAngleDegrees = 25.0;
 
         // distance from the center of the Limelight lens to the floor
-        double limelightLensHeightInches = 20.0;
+        double limelightLensHeightCentimeters = 20.0;
 
         // distance from the target to the floor
-        double goalHeightInches = 60.0;
+        double goalHeightCentimeters = 60.0;
 
         double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
         double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
 
         //calculate distance
-        double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)/Math.tan(angleToGoalRadians);
+        double distanceFromLimelightToGoalCentimeters = (goalHeightCentimeters - limelightLensHeightCentimeters)/Math.tan(angleToGoalRadians);
         
-        return distanceFromLimelightToGoalInches;
+        return distanceFromLimelightToGoalCentimeters;
     }
 
     // Called once the command ends or is interrupted.
