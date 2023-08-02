33daa1390a05361210b1d64f3c8f4e8e25350d42
Edan Liahovetsky <edan.liahovetsky@icloud.com>
2023-08-02 08:34:01 -0400

    paser finished!

--- src/main/java/frc/robot/commands/JsonChanger.java
+++ src/main/java/frc/robot/commands/JsonChanger.java
@@ -53,6 +53,18 @@ public class JsonChanger extends CommandBase {
 
     private PIDF drive;
     private PIDF angle;
+    private ShuffleboardTab tab = Shuffleboard.getTab("SwerveJson");
+    private GenericEntry driveP;
+    private GenericEntry driveI;
+    private GenericEntry driveD;
+    private GenericEntry driveF;
+    private GenericEntry driveIz;
+
+    private GenericEntry angleP;
+    private GenericEntry angleI;
+    private GenericEntry angleD;
+    private GenericEntry angleF;
+    private GenericEntry angleIz;
 
     public JsonChanger() throws IOException {
         String jsonContent = new String(Files.readAllBytes(Paths.get("/src/main/deploy/swerve/falcon/modules/pidfproperties.json")));
@@ -64,15 +76,34 @@ public class JsonChanger extends CommandBase {
         System.out.println("Drive: P = " + drive.p + ", I = " + drive.i + ", D = " + drive.d + ", F = " + drive.f + ", Iz = " + drive.iz);
         System.out.println("Angle: P = " + angle.p + ", I = " + angle.i + ", D = " + angle.d + ", F = " + angle.f + ", Iz = " + angle.iz);
 
-        // Modifying some values
-        drive.p = 0.05;
-        angle.f = 0.005;
+        //do the rest for all pid values
+        driveP = tab.add("driveP", driveP).getEntry();
+        driveI = tab.add("driveI", driveI).getEntry();
+        driveD = tab.add("driveD", driveD).getEntry();
+        driveF = tab.add("driveF", driveF).getEntry();
+        driveIz = tab.add("driveIz", driveIz).getEntry();
+        angleP = tab.add("angleP", angleP).getEntry();
+        angleI = tab.add("angleI", angleI).getEntry();
+        angleD = tab.add("angleD", angleD).getEntry();
+        angleF = tab.add("angleF", angleF).getEntry();
+        angleIz = tab.add("angleIz", angleIz).getEntry();
 
         
     }
     @Override
     public void execute() {
 
+        // Updating the values on Shuffleboard
+        driveP.setDouble(drive.p);
+        driveI.setDouble(drive.i);
+        driveD.setDouble(drive.d);
+        driveF.setDouble(drive.f);
+        driveIz.setDouble(drive.iz);
+        angleP.setDouble(angle.p);
+        angleI.setDouble(angle.i);
+        angleD.setDouble(angle.d);
+        angleF.setDouble(angle.f);
+        angleIz.setDouble(angle.iz);
 
         // Writing back to JSON
         JSONObject outputJson = new JSONObject();
@@ -83,8 +114,9 @@ public class JsonChanger extends CommandBase {
         try (FileWriter file = new FileWriter("file.json")) {
             file.write(outputJson.toString());
             System.out.println("Successfully Copied JSON Object to File...");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        
     }
     
 
