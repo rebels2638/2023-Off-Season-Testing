cab2570dbe1ce331eb9b839fc22960f450f0111a
Edan Liahovetsky <edan.liahovetsky@icloud.com>
2023-08-13 15:51:08 -0500

    gyro interface file creation

---
+++ src/main/java/frc/robot/subsystems/swervedrive/imu/Gyro.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.swervedrive.imu;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Gyro extends SubsystemBase {
+    
+}
---
+++ src/main/java/frc/robot/subsystems/swervedrive/imu/GyroIONaveX.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.swervedrive.imu;
+
+public class GyroIONaveX implements GyroIO{
+    
+}
